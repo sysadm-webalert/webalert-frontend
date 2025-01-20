c854a2e5c2d89a2242bcff25aee584313be44111
Víctor <39667448+VGzsysadm@users.noreply.github.com>
2025-01-20 15:16:50 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:8000/api/',
-  timeout: 50000,
+  timeout: 10000,
 });
 
 const register = async (registrationData) => {
